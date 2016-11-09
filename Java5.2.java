d2d3ca0d3150c2f7bca1d1c4ec642911fd36b0a6
Jason <fernlou007@yahoo.com>
2016-11-09 11:37:30 -0800

    week4 contribution1
    hangman

---
+++ Java5.2.java
@@ -0,0 +1,20 @@
+<!doctype>
+
+<html>
+<head>
+
+  <h1>Real estate Inquiry </h1>
+  <form action= "FromProcessor.html" method="get"
+  enctype="application/x-www-form-urlencoded">
+  <p>Name<br />size="50" /> </p>
+  <p>E-mail adress<br />
+  input type="text" name="E-mail" size="50" /> </p>
+
+
+
+
+
+
+
+
+</head>
